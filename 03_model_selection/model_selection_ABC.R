be481a8d19911f0f784ee01d9e03e811fbc58b12
Mauricio Montaño-Rendón <frida_shimka@dyn202228.shef.ac.uk>
2015-10-07 13:27:39 +0100

    New folder 03_model_selection contains R script used for estimating the posterior model probabilities

---
+++ 03_model_selection/model_selection_ABC.R
@@ -0,0 +1,53 @@
+####################################
+##### Model selection with ABC #####
+####################################
+
+# remove all objects
+rm(list = ls())
+
+# 1) import data
+
+    # load observed summary statistics (as vector)
+    obs_stat <- read.table(file="../ABCstat_observed_formatted.txt.gz", h=T)
+        # remove dataset column
+        obs_stat <- obs_stat[,-1]
+        # create vector of stats to be kept
+        STATS <- c(1:80, 109:116, 145:152, 181:188, 217:224, 253:260)
+        # keep only selected stats
+        obs_stat <- obs_stat[,STATS]
+    TARGET <- as.matrix(obs_stat)[1,]
+
+    # load simulated summary statistics (as matrix or data frame)
+        # null model
+        load('../01_null_NoMig_interspe/02_PLS_transformation/ABCstat_null_interspeNoMig.Rdata')
+        data_null <- stat
+        rm(stat, na_stat)
+        # full model
+        load('../02_full_Mig_interspe/02_PLS_transformation/ABCstat_full_interspeMig.Rdata')
+        data_full <- stat
+        rm(stat, na_stat)
+    SUMSTAT <- rbind(data_null, data_full)
+
+    # create a model index (as vector)
+        n_data_null <- nrow(data_null)
+        n_data_full <- nrow(data_full)
+    INDEX <- c(rep("null", n_data_null), rep("full", n_data_full))
+
+# 2) set parameters of ABC
+
+    # indicate required proportion of points nearest the target value
+    TOL <- 0.05
+
+    # indicate the type of simulation required
+    METHOD <- "neuralnet"
+
+# 3) estimate posterior model probabilities
+
+    # load abc package
+    library(abc)
+
+    # estimate posterior model probabilities
+    modsel <- postpr(target=TARGET, index=INDEX, sumstat=SUMSTAT, tol=TOL, method=METHOD)
+
+    # print model probabilities
+    modsel$pred
